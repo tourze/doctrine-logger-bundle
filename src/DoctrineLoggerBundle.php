9e25a94f222b6bf1410b0814b3c92efa77ab2d17
tourze-ci <you@example.com>
2025-04-12 04:54:03 +0000

    feat: 添加异常处理忽略文件

--- src/DoctrineLoggerBundle.php
+++ src/DoctrineLoggerBundle.php
@@ -4,6 +4,10 @@ namespace Tourze\DoctrineLoggerBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Tourze\BacktraceHelper\Backtrace;
+use Tourze\DoctrineLoggerBundle\Middleware\LogConnection;
+use Tourze\DoctrineLoggerBundle\Middleware\LogDriver;
+use Tourze\DoctrineLoggerBundle\Middleware\LogMiddleware;
+use Tourze\DoctrineLoggerBundle\Middleware\LogStatement;
 use Tourze\DoctrineLoggerBundle\Service\QueryExecutionTimeLogger;
 
 class DoctrineLoggerBundle extends Bundle
@@ -13,5 +17,9 @@ class DoctrineLoggerBundle extends Bundle
         parent::boot();
 
         Backtrace::addProdIgnoreFiles((new \ReflectionClass(QueryExecutionTimeLogger::class))->getFileName());
+        Backtrace::addProdIgnoreFiles((new \ReflectionClass(LogConnection::class))->getFileName());
+        Backtrace::addProdIgnoreFiles((new \ReflectionClass(LogDriver::class))->getFileName());
+        Backtrace::addProdIgnoreFiles((new \ReflectionClass(LogMiddleware::class))->getFileName());
+        Backtrace::addProdIgnoreFiles((new \ReflectionClass(LogStatement::class))->getFileName());
     }
 }
