b1dcd707c3a854a9205b652cc51645fe357660a6
tourze-ci <you@example.com>
2025-04-12 03:50:24 +0000

    chore: 添加 DoctrineLoggerBundle 启动时忽略文件过滤

--- src/DoctrineLoggerBundle.php
+++ src/DoctrineLoggerBundle.php
@@ -3,7 +3,15 @@
 namespace Tourze\DoctrineLoggerBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Tourze\BacktraceHelper\Backtrace;
+use Tourze\DoctrineLoggerBundle\Service\QueryExecutionTimeLogger;
 
 class DoctrineLoggerBundle extends Bundle
 {
+    public function boot(): void
+    {
+        parent::boot();
+
+        Backtrace::addProdIgnoreFiles((new \ReflectionClass(QueryExecutionTimeLogger::class))->getFileName());
+    }
 }
